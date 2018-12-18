662a7f9383d7d9cd84c6ef84f86e5796b0458ca3
Glitch (react-email-starting-point) <none>
2018-12-18 19:52:57 +0000

    🐙💼 Checkpoint ./src/components/email.js:2232060/281

--- src/components/email.js
+++ src/components/email.js
@@ -3,14 +3,16 @@ import Sidebar from './sidebar';
 import SingleEmail from './single-email';
 import EmailList from './email-list';
 import './email.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom'; 
 
 export default function Email() {
     return (
         <div className="email">
           <Router>
             <div>
-              <Route exact path
+              <Redirect from="/" to="inbox" />
+              <Route exact path = "/:folderId" component= {EmailList} /> 
+              <Route exact path = "/:folderId/:emailId" component= { SingleEmail } /> 
             <Sidebar />
             <main>
                 <EmailList folderId="inbox" />
